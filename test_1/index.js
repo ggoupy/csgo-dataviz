f02021930b5af9cd437c95305d83b9e32f4762e5
Yuri Gardinazzi <gardinazzi.yuri@gmail.com>
2021-12-14 16:45:39 +0100

    beginning to test svg plots (upload of files)

--- test_1/index.js
+++ test_1/index.js
@@ -1,1 +1,14 @@
-console.log("Hello world >:C");
+
+//Usefull links http://zevross.com/blog/2019/08/20/load-external-svgs-with-d3-v5/
+
+
+console.log("Let's try to read the data");
+
+
+
+d3.csv("game.csv",function(data){
+    //for(i = 0; i < data.length; i ++){
+      console.log(data);
+    //}
+    
+});
