1c2f2e884c1dc6d682195c442557b05b8fd938eb
Yuri Gardinazzi <gardinazzi.yuri@gmail.com>
2021-12-29 22:20:06 +0100

    very basic working idea - still a lot of refactoring to do

--- test_1/index.js
+++ test_1/index.js
@@ -2,7 +2,6 @@
 //Usefull links http://zevross.com/blog/2019/08/20/load-external-svgs-with-d3-v5/
 //http://jsfiddle.net/u3tjmy0b/4/
 //https://stackoverflow.com/questions/12975929/how-to-use-svg-file-for-image-source-in-d3
-console.log("Let's try to read the data");
 
 
 //data upload
@@ -15,20 +14,31 @@ var dataset;
 });*/
 
 var svgImg= d3.select("#test").append("img")
-              .attr("src","gun-svgrepo-com.svg")
-              .attr("width", 100)
-              .attr("height", 100)
+              .attr("src","128px-Gun_outline.png");
+              //.attr("width", 100)
+              //.attr("height", 100);
 
+/*
 // Define the gradient
 var gradient = svgImg.append("svg:defs")
         .append("svg:linearGradient")
         .attr("id", "gradient")
-        .attr("y1", "100%")
+        .attr("y1", "0%")
         .attr("y2", "0%")
         .attr("spreadMethod", "pad");
 
         // Define the gradient colors
     gradient.append("svg:stop")
+            .attr("id","stop")
+            .attr("offset", "50%")
+            .attr("stop-color", "orange");
+            //.attr("stop-opacity", 1);
+    gradient.append("svg:stop")
+            .attr("id","stop1")
             .attr("offset", "0%")
-            .attr("stop-color", "orange")
-            .attr("stop-opacity", 1);    
+            .attr("stop-color", "lightblue");
+            //.attr("stop-opacity", 1);*/    
+    
+var data = { percent: 5.0 };
+
+
