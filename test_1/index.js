49b7a9f98a7a6b5cc23a4aacb110734566dce325
Yuri Gardinazzi <gardinazzi.yuri@gmail.com>
2022-01-06 12:12:09 +0100

    add reading of the data

--- test_1/index.js
+++ test_1/index.js
@@ -1,22 +1,37 @@
 
 
-//data upload
-var dataset;
-/*d3.csv("game.csv",function(data){
-    //for(i = 0; i < data.length; i ++){
-      console.log(data);
-    //}
-    dataset = data;
-});*/
+var weapons = ["AK-47","AWP","GALIL","GLOCK-17","M4A1-S","M4A4","SSG08","USP"];
+ function readData(){
+  d3.csv("output.csv").then(function(d){
+    var precision_weapon = {}
+    for(var i = 0; i < d.length; i++){
+      if(d[i].Pseudo === "LaMasse" && d[i].Game_number==3 && d[i].Round_number == 2){
+        weapons.forEach(function(weapon){
+          fired = d[i]["shots_fired_"+weapon];
+          hit = d[i]["shots_hit_"+weapon];
+          console.log("shots_fired_"+weapon+" "+ d[i]["shots_hit_"+weapon]); 
+          if((hit/fired) >= 0){
+            precision_weapon[weapon] = {'precision': hit/fired, 'hit': hit, "fired":fired};
+          }
+
+        });
+        console.log(precision_weapon);
+      }
+    }
+    
+  });
+ 
+}
 
 function addWeapons(){
-  var weapons = ["AK-47.png","AWP.png","GALIL.png","GLOCK-17.png","M4A1-S.png","M4A4.png","SSG08.png","USP.png"];
+  //var weapons = ["AK-47.png","AWP.png","GALIL.png","GLOCK-17.png","M4A1-S.png","M4A4.png","SSG08.png","USP.png"];
+
   weapons.forEach(function(item,index,array){
     var graph = d3.select("#weapon_chart").append("div")
       .attr("id","bar_"+index)
       .attr("class","progress")
       .append("img")
-      .attr("src","weapons/"+item)
+      .attr("src","weapons/"+item+".png")
       .attr("id","weapon_"+index);
     $("#bar_"+index).width($("#weapon_"+index).width());
     $("#bar_"+index).height($("#weapon_"+index).height());
@@ -24,15 +39,6 @@ function addWeapons(){
   });
 }
 
-function setBackground(){
-  console.log("mi han chiamato");
-  var svgImg= d3.select("#test").append("img")
-              .attr("src","weapons/M4A1-S.png")
-              .attr("id","img_gun");
-
-  $("#test").width($('#img_gun').width());
-  $("#test").height($('#img_gun').height());
-};
 function changePercentage(){
         for(i = 0; i < 8; i++){
           var width = $('#weapon_'+i).width();
@@ -40,13 +46,12 @@ function changePercentage(){
           width *=Math.random();
           $("#bar_"+i).width(width);
           $("#bar_"+i).height(height);   
-        }
+}
 
         
 }
 addWeapons();
-//setBackground();
-    
+readData();
 var data = { percent: 5.0 };
 
 
