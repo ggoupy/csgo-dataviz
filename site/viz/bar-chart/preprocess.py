f098a01b677906b5d5f4ec68d542d53cc1cd1f2b
Gaspard GOUPY <gaspard.goupy@etu.univ-lyon1.fr>
2022-01-09 13:54:04 +0100

    Fix csv

--- site/viz/bar-chart/preprocess.py
+++ site/viz/bar-chart/preprocess.py
@@ -1,7 +1,6 @@
 import pandas as pd
 
-data = pd.read_csv('../../../data/dataset/output.csv', sep=',') 
-dataRound = pd.read_csv('../../../data/dataset/outputRound.csv', sep=',') 
+data = pd.read_csv('../../../data/dataset/preprocessed/output.csv', sep=',') 
 
 def filter_row(data, kv):
     data_new = data
@@ -9,12 +8,6 @@ def filter_row(data, kv):
         data_new = data_new[data_new[key] == value]
     return data_new
 
-#game_number = 6
-#pseudo = "LaMasse"
-#data = filter_row(data,[
-    #("Game_number",game_number),
-    #("Pseudo",pseudo),
-#])
 
 data = data[["Game_number","Round_number","Team","dmg","Pseudo","damage_team","damage_ennemy_team", "dmg_received", "ennemies_flashed", "shots_fired", "shots_hit", "hits_head", "kills"]]
 data = data.sort_values(by="Round_number")
